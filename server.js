04aeff3a4bb2300224880d6a9a0c9adb29b3d771
Brandon G.M <brandongmerritt@gmail.com>
2021-05-19 15:58:51 -0400

    app express

--- server.js
+++ server.js
@@ -6,3 +6,10 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
