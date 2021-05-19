84b058474d4e0df3d9d317dbe5b13dc11caa7601
Brandon G.M <brandongmerritt@gmail.com>
2021-05-19 15:35:12 -0400

    added name

--- models/excercise.js
+++ models/excercise.js
@@ -4,7 +4,9 @@ const Schema = mongoose.Schema;
 
 const ExerciseSchema = new Schema({
 	// creates type attribute
-	type: String,
+    type: String,
+    	// creates name attribute
+	name: String,
 });
 
 const Exercise = mongoose.model("Exercise", ExerciseSchema);
